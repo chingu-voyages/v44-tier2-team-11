65da57ac7c8b516b76844f7dd34b9dc7962d4678
sceiiya <124661065+sceiiya@users.noreply.github.com>
2023-05-17 17:18:43 +0800

    comment out App Leaderboard component test

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
-// import Arena from './components/arena/Arena';
+import Arena from './components/arena/Arena';
 import Leaderboard from './components/leaderboard/Leaderboard';
-import BotBlue from "./components/bots/BotBlue";
 
 
-// function App() {
-//   return (
-//     <>
-//       <h2 className="justify-centertext-4xl flex h-screen w-full items-center font-black text-form-500">
-//         Start Coding Here
-//       </h2>
-//     </>
-//   );
-// }
-
-// export default App;
-
-const App = () => {
+function App() {
   return (
     <>
-    <Leaderboard />
+      <h2 className="justify-centertext-4xl flex h-screen w-full items-center font-black text-form-500">
+        Start Coding Here
+      </h2>
     </>
   );
 }
+
 export default App;
+
+// const App = () => {
+//   return (
+//     <>
+//     <Leaderboard />
+//     </>
+//   );
+// }
+// export default App;
