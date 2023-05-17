1ea076fe0097c0349c9f2bc649f1888b51c18154
sceiiya <124661065+sceiiya@users.noreply.github.com>
2023-05-17 17:18:02 +0800

    add dynamic Bot figutr component

---
+++ src/components/bots/BotFig.jsx
@@ -0,0 +1,272 @@
+// prop:
+// priColor //primary color of the bot fig
+// bColor //border/stroke color of the bot fig
+
+function BotFig(prop) {
+  return (
+    <>
+      <svg
+        className="translate-y-1"
+        width={prop.scale}
+        height={prop.scale}
+        viewBox="0 0 542 507"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g filter="url(#filter0_i_65_74)">
+          <rect
+            x="0.138184"
+            y="290.713"
+            width="53.8262"
+            height="128.585"
+            rx="26.9131"
+            fill={ prop.priColor }
+            fillOpacity="0.962"
+          />
+        </g>
+        <rect
+          x="10.1382"
+          y="300.713"
+          width="33.8262"
+          height="108.585"
+          rx="16.9131"
+          stroke={ prop.bColor }
+          strokeWidth="20"
+        />
+        <rect
+          x="497.564"
+          y="300.713"
+          width="33.8262"
+          height="108.585"
+          rx="16.9131"
+          fill={ prop.priColor }
+          fillOpacity="0.962"
+          stroke={ prop.bColor }
+          strokeWidth="20"
+        />
+        <line
+          x1="276.279"
+          y1="87.3687"
+          x2="276.279"
+          y2="192.031"
+          stroke={ prop.bColor }
+          strokeWidth="20"
+        />
+        <g filter="url(#filter1_i_65_74)">
+          <rect
+            x="206.471"
+            y="141.195"
+            width="131.575"
+            height="77.749"
+            rx="38.8745"
+            fill={ prop.priColor }
+            fillOpacity="0.962"
+          />
+        </g>
+        <rect
+          x="216.471"
+          y="151.195"
+          width="111.575"
+          height="57.749"
+          rx="28.8745"
+          stroke={ prop.bColor }
+          strokeWidth="20"
+        />
+        <g filter="url(#filter2_i_65_74)">
+          <circle
+            cx="272.26"
+            cy="48.4944"
+            r="47.8455"
+            fill={ prop.priColor }
+            fillOpacity="0.962"
+          />
+        </g>
+        <circle
+          cx="272.26"
+          cy="48.4944"
+          r="37.8455"
+          stroke={ prop.bColor }
+          strokeWidth="20"
+        />
+        <g filter="url(#filter3_i_65_74)">
+          <path
+            d="M27.052 399.678C27.052 280.044 124.035 183.061 243.67 183.061H297.86C417.495 183.061 514.478 280.044 514.478 399.678V399.678C514.478 458.408 466.868 506.018 408.139 506.018H133.392C74.6619 506.018 27.052 458.408 27.052 399.678V399.678Z"
+            fill={ prop.priColor }
+            fillOpacity="0.962"
+          />
+        </g>
+        <path
+          d="M243.67 193.061H297.86C411.972 193.061 504.478 285.566 504.478 399.678C504.478 452.885 461.346 496.018 408.139 496.018H133.392C80.1848 496.018 37.052 452.885 37.052 399.678C37.052 285.566 129.558 193.061 243.67 193.061Z"
+          stroke={ prop.bColor }
+          strokeWidth="20"
+        />
+        <circle
+          cx="167.599"
+          cy="296.694"
+          r="55.7876"
+          fill="white"
+          stroke={ prop.bColor }
+          strokeWidth="20"
+        />
+        <circle cx="167.601" cy="296.694" r="32.8938" fill="#0C0C0C" />
+        <circle cx="155.638" cy="290.713" r="11.9614" fill="white" />
+        <circle
+          cx="370.941"
+          cy="296.694"
+          r="55.7876"
+          fill="white"
+          stroke={ prop.bColor }
+          strokeWidth="20"
+        />
+        <circle cx="370.941" cy="296.694" r="32.8938" fill="#0C0C0C" />
+        <circle cx="358.978" cy="290.713" r="11.9614" fill="white" />
+        <path
+          d="M212.454 401.355C236.376 447.705 303.659 447.705 329.077 401.355"
+          stroke={ prop.bColor }
+          strokeWidth="20"
+          strokeLinecap="round"
+        />
+        <defs>
+          <filter
+            id="filter0_i_65_74"
+            x="0.138184"
+            y="290.713"
+            width="53.8262"
+            height="128.585"
+            filterUnits="userSpaceOnUse"
+            colorInterpolationFilters="sRGB"
+          >
+            <feFlood floodOpacity="0" result="BackgroundImageFix" />
+            <feBlend
+              mode="normal"
+              in="SourceGraphic"
+              in2="BackgroundImageFix"
+              result="shape"
+            />
+            <feColorMatrix
+              in="SourceAlpha"
+              type="matrix"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+              result="hardAlpha"
+            />
+            <feOffset dx="34.426" dy="12.9098" />
+            <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+            <feColorMatrix
+              type="matrix"
+              values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.25 0"
+            />
+            <feBlend
+              mode="normal"
+              in2="shape"
+              result="effect1_innerShadow_65_74"
+            />
+          </filter>
+          <filter
+            id="filter1_i_65_74"
+            x="206.471"
+            y="141.195"
+            width="131.575"
+            height="77.749"
+            filterUnits="userSpaceOnUse"
+            colorInterpolationFilters="sRGB"
+          >
+            <feFlood floodOpacity="0" result="BackgroundImageFix" />
+            <feBlend
+              mode="normal"
+              in="SourceGraphic"
+              in2="BackgroundImageFix"
+              result="shape"
+            />
+            <feColorMatrix
+              in="SourceAlpha"
+              type="matrix"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+              result="hardAlpha"
+            />
+            <feOffset dx="34.426" dy="12.9098" />
+            <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+            <feColorMatrix
+              type="matrix"
+              values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.25 0"
+            />
+            <feBlend
+              mode="normal"
+              in2="shape"
+              result="effect1_innerShadow_65_74"
+            />
+          </filter>
+          <filter
+            id="filter2_i_65_74"
+            x="224.414"
+            y="0.648926"
+            width="95.6909"
+            height="95.6909"
+            filterUnits="userSpaceOnUse"
+            colorInterpolationFilters="sRGB"
+          >
+            <feFlood floodOpacity="0" result="BackgroundImageFix" />
+            <feBlend
+              mode="normal"
+              in="SourceGraphic"
+              in2="BackgroundImageFix"
+              result="shape"
+            />
+            <feColorMatrix
+              in="SourceAlpha"
+              type="matrix"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+              result="hardAlpha"
+            />
+            <feOffset dx="34.426" dy="12.9098" />
+            <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+            <feColorMatrix
+              type="matrix"
+              values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.25 0"
+            />
+            <feBlend
+              mode="normal"
+              in2="shape"
+              result="effect1_innerShadow_65_74"
+            />
+          </filter>
+          <filter
+            id="filter3_i_65_74"
+            x="27.052"
+            y="183.061"
+            width="487.426"
+            height="322.957"
+            filterUnits="userSpaceOnUse"
+            colorInterpolationFilters="sRGB"
+          >
+            <feFlood floodOpacity="0" result="BackgroundImageFix" />
+            <feBlend
+              mode="normal"
+              in="SourceGraphic"
+              in2="BackgroundImageFix"
+              result="shape"
+            />
+            <feColorMatrix
+              in="SourceAlpha"
+              type="matrix"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+              result="hardAlpha"
+            />
+            <feOffset dx="34.426" dy="12.9098" />
+            <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+            <feColorMatrix
+              type="matrix"
+              values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.25 0"
+            />
+            <feBlend
+              mode="normal"
+              in2="shape"
+              result="effect1_innerShadow_65_74"
+            />
+          </filter>
+        </defs>
+      </svg>
+    </>
+  );
+}
+
+export default BotFig;
