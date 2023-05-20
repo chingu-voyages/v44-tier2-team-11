59b6114a0ca012ef3c2e3347c84e2770c0e90dd6
Finestwork <Younes.Espiritu@outlook.com>
2023-05-21 02:52:32 +0800

    Create an active for all buttons in operation form field

--- src/components/forms/OperationForm.jsx
+++ src/components/forms/OperationForm.jsx
@@ -1,17 +1,34 @@
-import OperationFormButton from '../base/OperationFormButton';
+// NPM
+import { useState } from 'react';
 
 const OperationForm = () => {
   const OPERATIONS = ['AND', 'OR', 'XOR', 'NAND', 'NOR', 'XNOR'];
+  const [operation, setOperation] = useState('AND');
+
+  const changeOperation = (e) => {
+    const BTN = e.currentTarget;
+    const IND = +BTN.dataset.index;
+    setOperation(OPERATIONS[IND]);
+  };
+
   return (
     <div className="flex flex-col">
       <span className="mb-2 text-sm font-black text-form-900">Operation:</span>
       <div>
-        {OPERATIONS.map((operation) => (
-          <OperationFormButton
-            key={operation}
-            operation={operation}
-            className="mr-3 last-of-type:mr-0"
-          />
+        {OPERATIONS.map((op, ind) => (
+          <button
+            key={ind}
+            type="button"
+            data-index={ind}
+            className={`mr-2 rounded-lg px-4 py-2 text-xs font-black outline-none transition-shadow duration-100 ease-in last-of-type:mr-0 focus:ring-4 focus:ring-primary-300 ${
+              operation === OPERATIONS[ind]
+                ? 'bg-primary-50 text-primary-500 hover:bg-primary-100 focus:bg-primary-100'
+                : 'bg-form-300 text-form-600 hover:bg-form-400 focus:bg-primary-50 focus:text-primary-500'
+            }`}
+            onClick={changeOperation}
+          >
+            {op}
+          </button>
         ))}
       </div>
     </div>
