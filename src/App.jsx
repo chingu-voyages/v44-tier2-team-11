f77b0c9184d14cdb3e0644ddcb68c200e2d2c07e
maxwell wong <midorikawamaxwell@gmail.com>
2023-05-21 01:05:00 +0900

    added props and with and height class to wrench and trophy icon so that their svg can be resized

--- src/App.jsx
+++ src/App.jsx
@@ -9,19 +9,24 @@ function App() {
   return (
     <>
       <div className="flex h-screen items-center justify-center">
-      <div className="w-3/4 rounded bg-gradient-to-b from-red-100 to-red-300 p-4">
-        <div className="flex items-center justify-between pt-4 px-4">
-          <LogoIcon />
-          <div>
-            <button className="h-12 w-12 rounded-full bg-red-500 text-white"><TrophyStar /></button>
-            <button className="h-12 w-12 rounded-full bg-red-500 text-white"><WrenchIcon/></button>
+        <div className="w-3/4 rounded bg-gradient-to-b from-red-100 to-red-300 p-4">
+          <div className="flex items-center justify-between pt-4 px-4">
+            <div className='flex items-center text-white bold text-xl'>
+              <LogoIcon className="w-8 h-8"/> Boole Bot
+            </div>
+            <div>
+              <button className="flex items-center justify-center h-14 w-14 rounded-full bg-red-500 text-white items-center justify-center">
+                <TrophyStar size={10}/>
+              </button>
+              <button className="h-14 w-14 rounded-full bg-red-500 text-white"><WrenchIcon/></button>
+            </div>
+          </div>
+          <div className="flex w-full justify-center">
+            <button className="relative h-16 w-72 rounded-full bg-red-500 font-bold text-white shadow-lg my-10 text-2xl">START!</button>
           </div>
         </div>
-        <div className="flex w-full justify-center">
-          <button className="relative h-16 w-72 rounded-full bg-red-500 font-bold text-white shadow-lg my-10 text-2xl">START!</button>
-        </div>
       </div>
-    </div>
+    <Arena />
     </>
   );
 }
--- src/components/arena/CircleButton.jsx
+++ src/components/arena/CircleButton.jsx
@@ -1,8 +1,7 @@
-import LogoIcon from './components/icons/LogoIcon'
 
 const CircleButton = ({ icon }) => {
   return (
-    <button className="bg-red-400 rounded-full p-2">
+    <button className="bg-red-400 rounded-full p-5 w-4">
       {icon}
     </button>
   );
