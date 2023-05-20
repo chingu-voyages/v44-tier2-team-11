07a3bf1da103b6e91749288317273d7c8d81103c
maxwell wong <midorikawamaxwell@gmail.com>
2023-05-21 01:25:25 +0900

    changed the size of the logo icon and name text

--- src/App.jsx
+++ src/App.jsx
@@ -11,15 +11,16 @@ function App() {
       <div className="flex h-screen items-center justify-center">
         <div className="w-3/4 rounded bg-gradient-to-b from-red-100 to-red-300 p-4">
           <div className="flex items-center justify-between pt-4 px-4">
-            <div className='flex items-center text-white bold text-xl'>
-              <LogoIcon size={80}/> Boole Bot
+            <div className='flex items-center'>
+              <LogoIcon size={60} />
+              <div className='text-2xl font-bold text-270000 ml-4'>Boole Bot</div>
             </div>
             <div className='flex flex-row'>
-              <button className="flex items-center justify-center h-14 w-14 rounded-full bg-red-500 text-white">
-                <TrophyStar size={10}/>
+              <button className="flex items-center justify-center h-14 w-14 rounded-full bg-red-500 text-white mr-4">
+                <TrophyStar size={8}/>
               </button>
               <button className="flex items-center justify-center h-14 w-14 rounded-full bg-red-500 text-white">
-                <WrenchIcon size={10}/>
+                <WrenchIcon size={8}/>
               </button>
             </div>
           </div>
