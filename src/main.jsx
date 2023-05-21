41bc5b87a24a08001092671207da489feba71f97
Finestwork <Younes.Espiritu@outlook.com>
2023-05-21 13:52:07 +0800

    Remove comments

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,7 @@ import './index.css';
 import AppContext from './contex.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <AppContext>
-    <App />
-  </AppContext>
-  // </React.StrictMode>
+    <AppContext>
+        <App/>
+    </AppContext>
 );
