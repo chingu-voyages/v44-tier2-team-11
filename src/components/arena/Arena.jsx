d94b841fa78d17df37745918ce2b0cf8b27c9c2c
VinhQua <dragonlinuxnoinochi@gmai.com>
2023-05-21 09:27:09 +0900

    Move start button from arena to nav component

--- src/components/arena/Arena.jsx
+++ src/components/arena/Arena.jsx
@@ -4,17 +4,10 @@ import Bot from './Bot';
 import { useGlobalContext } from '../../contex';
 
 const Arena = () => {
-  const [inGame, setInGame] = useState(false);
-  const { botList } = useGlobalContext();
-  console.log(inGame);
+  const { botList, inGame } = useGlobalContext();
+
   return (
     <>
-      <button
-        onClick={() => setInGame(!inGame)}
-        className="bold m-20 rounded-full bg-primary-100 p-2"
-      >
-        {inGame ? 'Pause' : 'Start'}
-      </button>
       <div className="relative flex w-8x8 flex-wrap bg-primary-100">
         {botList.map((bot) => (
           <Bot key={bot.id} {...bot} inGame={inGame} />
