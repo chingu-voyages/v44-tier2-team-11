2f623369d1961c57d1f7c1aba4c784ca57cab326
Finestwork <Younes.Espiritu@outlook.com>
2023-06-04 14:06:31 +0800

    Change button text

--- src/components/shared/arena/ArenaControlButton.jsx
+++ src/components/shared/arena/ArenaControlButton.jsx
@@ -16,7 +16,7 @@ const ArenaControlButton = () => {
         onClick={onClickToggleBattle}
         className="relative my-10 w-full max-w-[220px] rounded-full bg-primary-500 py-4 text-base font-black text-white shadow-[inset_0_-6px_0_0_rgba(0,0,0,0.16)] outline-none transition-all duration-100 ease-in hover:bg-primary-600/75 focus:translate-y-1 focus:bg-primary-600/75 focus:shadow-[inset_0_-3px_0_0_rgba(0,0,0,0.16)]"
       >
-        {inGame ? 'PAUSE' : 'START!'}
+        {inGame ? 'Stop!' : 'Battle!'}
       </button>
     </div>
   );
