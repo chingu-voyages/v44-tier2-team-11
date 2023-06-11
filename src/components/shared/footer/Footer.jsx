88b0935a46e45338b4cc2ff987e78ee2d2fafa9e
sceiiya <124661065+sceiiya@users.noreply.github.com>
2023-06-11 21:21:07 +0800

    add footer

---
+++ src/components/shared/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+    return(
+        <div className='h-6 w-full flex content-center text-center bg-[#270000]'>
+            <a className='m-auto text-white font-extrabold italic text-xs' href='https://github.com/chingu-voyages/v44-tier2-team-11/' target='_blank'>Meet the Creators!</a>
+        </div>
+    )
+}
+
+export default Footer;
