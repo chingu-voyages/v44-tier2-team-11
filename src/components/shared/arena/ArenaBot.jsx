3c402152a62ebffe999cbba1d3da61556e9ebad9
sceiiya <124661065+sceiiya@users.noreply.github.com>
2023-06-11 16:22:07 +0800

    add tie in global bot state and fix toast position

--- src/components/shared/arena/ArenaBot.jsx
+++ src/components/shared/arena/ArenaBot.jsx
@@ -42,7 +42,7 @@ const Bot = ({
   const configSpeed = configuration?.speed ?? 1000;
 
   // console.log(botScores);
-  console.log(inGamePositions);
+  // console.log(inGamePositions);
 
   useEffect(() => {
     let timeId;
@@ -61,7 +61,7 @@ const Bot = ({
   // console.log(botScores.current);
   //update bot scores for the first time
   useEffect(() => {
-    const bot = { id, name, win: 0, lose: 0, colorSchemes };
+    const bot = { id, name, win: 0, lose: 0, tie: 0, colorSchemes };
     botScores.current = [...botScores.current, bot];
   }, []);
 
@@ -130,7 +130,9 @@ const Bot = ({
                 ...botScores.current.filter((bot) => bot.id !== id),
                 botScore,
               ];
-              toast.success(`${name} won`);
+              toast.success(`${name} won`, {
+                position: toast.POSITION.TOP_LEFT,
+              });
               colliedCheck.current = true;
             }, 1995);
           } else if (result === 0) {
@@ -141,8 +143,12 @@ const Bot = ({
               setIsCollied(() => {
                 return false;
               });
+              const botScore = botScores.current.find((bot) => bot.id === id);
+              botScore.tie = botScore.tie + 1;
               //keep bot moving
-              toast.success(`${name} vs ${botName} tied`);
+              toast.success(`${name} vs ${botName} tied`, {
+                position: toast.POSITION.TOP_LEFT,
+              });
               console.log('tie');
             }, 2000);
             CheckColliedTime = setTimeout(() => {
@@ -167,7 +173,9 @@ const Bot = ({
 
               setBotOver(true);
 
-              toast.success(`${name} lost`);
+              toast.success(`${name} lost`, {
+                position: toast.POSITION.TOP_LEFT,
+              });
               inGamePositions.current = inGamePositions.current.filter(
                 (bot) => bot.id !== id
               );
