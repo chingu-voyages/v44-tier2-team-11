4baca487b2f1395195bb2f2f336d655cbb60a38b
sceiiya <124661065+sceiiya@users.noreply.github.com>
2023-06-11 23:18:22 +0800

    fix leaderboard align things

--- src/components/shared/modals/leaderboards-panel/LeaderboardHeader.jsx
+++ src/components/shared/modals/leaderboards-panel/LeaderboardHeader.jsx
@@ -2,20 +2,17 @@ import React, { useContext } from "react";
 
 const LeaderboardHeader = () => {
     return(
-        <div className=" flex flex-row justify-evenly h-min my-4">
-            {/* <div className="flex justify-center items-center">
-                Standing
-            </div> */}
-            <div className="flex justify-center items-center font-bold">
+        <div className=" flex flex-row justify-evenly h-min my-1">
+            <div className="flex items-center justify-center w-16 font-bold">
                 Rank
             </div>
-            <div className="flex justify-center items-center font-bold">
+            <div className="flex items-center justify-center w-16 font-bold">
                 Bot
             </div>
-            <div className="flex justify-center items-center font-bold">
+            <div className="flex items-center justify-center w-16 font-bold">
                 Win
             </div>
-            <div className="flex justify-center items-center font-bold">
+            <div className="flex items-center justify-center w-16 font-bold">
                 Loss
             </div>
     </div>
