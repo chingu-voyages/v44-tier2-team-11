161d576e82e4f8de21c01b59b68e39bd8d0fff77
VinhQua <dragonlinuxnoinochi@gmai.com>
2023-06-14 16:13:58 +0900

    remove test data

--- src/components/shared/arena/ArenaControlButton.jsx
+++ src/components/shared/arena/ArenaControlButton.jsx
@@ -14,7 +14,7 @@ const ArenaControlButton = () => {
     <div className="flex w-full justify-center">
       <button
         onClick={onClickToggleBattle}
-        // disabled={bots.length < 2}
+        disabled={bots.length < 2}
         className="relative my-10 w-full max-w-[220px] rounded-full bg-primary-500 py-4 text-base font-black text-white shadow-[inset_0_-6px_0_0_rgba(0,0,0,0.16)] outline-none transition-all duration-100 ease-in hover:bg-primary-600/75 focus:translate-y-1 focus:bg-primary-600/75 focus:shadow-[inset_0_-3px_0_0_rgba(0,0,0,0.16)] disabled:cursor-not-allowed"
       >
         {inGame ? 'Stop!' : 'Battle!'}
